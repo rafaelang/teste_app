b062288b273be4bc9f4ee47c0a7ad7f44c507496
Rafael Garcia <rafael.garcia@zoop.com.br>
2020-03-04 19:29:07 -0300

    inicial

--- teste_app/search.py
+++ teste_app/search.py
@@ -3,6 +3,11 @@ import requests
 from teste_app import settigns
 
 
+def get_page():
+	with open("teste.html") as fp:
+		return fp.read()
+
+
 def google(q: str):
     """Faz uma pesquisa no google"""
 
