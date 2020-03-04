55ffb8af020ea0341c65d7539a51909fd6cb544c
Rafael Garcia <rafael.garcia@zoop.com.br>
2020-03-04 19:46:01 -0300

    package_data

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setup(
     packages=find_packages(),
     install_requires=["requests"],
     test_suite='test',
-    include_package_data=True
+    include_package_data=True,
+    package_data={'': ['teste.html']},
 )
