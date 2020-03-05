d77b7c84c3c9a0569e68f4b3d8f228a3781858c8
Rafael Garcia <rafa.informatica@gmail.com>
2020-03-05 13:03:33 -0300

    teste

--- teste_app/search.py
+++ teste_app/search.py
@@ -7,7 +7,12 @@ from teste_app import settigns
 
 def get_page():
 	with open("%s/teste.html" % os.path.dirname(__file__)) as fp:
-		return fp.read()
+		data = fp.read()
+
+    with open("%s/tempaltes/template1.html" % os.path.dirname(__file__)) as fp:
+        data2 = fp.read()
+
+    return data, data2
 
 
 def google(q: str):
