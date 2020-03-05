1407de8f78a6bf66947e8c0e1eb96282eaaa664a
Rafael Garcia <rafa.informatica@gmail.com>
2020-03-05 13:06:40 -0300

    fix

--- teste_app/search.py
+++ teste_app/search.py
@@ -6,8 +6,8 @@ from teste_app import settigns
 
 
 def get_page():
-	with open("%s/teste.html" % os.path.dirname(__file__)) as fp:
-		data = fp.read()
+    with open("%s/teste.html" % os.path.dirname(__file__)) as fp:
+        data = fp.read()
 
     with open("%s/tempaltes/template1.html" % os.path.dirname(__file__)) as fp:
         data2 = fp.read()
